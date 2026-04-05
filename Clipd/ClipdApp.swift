1932f37ee93ed7003875501b3ebb89a3e98aab50
drfarr <hi@danfarr.com>
2026-04-05 10:00:00 +0100

    feat: add ClipdApp entry point with menu bar app delegate

---
+++ Clipd/ClipdApp.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+@main
+struct ClipdApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        false
+    }
+}
