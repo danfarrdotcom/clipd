88ccd6b8e0dc7a9f57cd4b5c6302a8ba38327b10
drfarr <hi@danfarr.com>
2026-04-08 14:00:00 +0100

    refactor: wire AppDelegate with CaptureManager and MenuBarManager

--- Clipd/ClipdApp.swift
+++ Clipd/ClipdApp.swift
@@ -12,8 +12,13 @@ struct ClipdApp: App {
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var captureManager: CaptureManager!
+    private var menuBarManager: MenuBarManager!
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
+        captureManager = CaptureManager()
+        menuBarManager = MenuBarManager(captureManager: captureManager)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
